19f3973d3a43880e6afe89682d0e32e7b4c0e24e
Hammam <hammam.mahfuzh.s@mail.ugm.ac.id>
2022-09-05 01:21:00 +0700

    A function was created to compare user's hand choices

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -49,4 +49,19 @@ class App {
         }
         return theInput
     }
+
+    private fun fight(listOfPlayers: ArrayList<Player>): String {
+        val choice1: Int = Player().possibleChoices.indexOf(listOfPlayers[0].hand)
+        val choice2: Int = Player().possibleChoices.indexOf(listOfPlayers[1].hand)
+        val playerOneWins: Boolean =
+            (choice1 == 0 && choice2 == 2) || (choice1 == 1 && choice2 == 0) || (choice1 == 2 && choice2 == 1)
+        //val draw: Boolean = listOfPlayers[0].hand == listOfPlayers[1].hand
+        val playerTwoWins: Boolean =
+            (choice1 == 2 && choice2 == 0) || (choice1 == 1 && choice2 == 2) || (choice1 == 0 && choice2 == 1)
+        return when {
+            playerOneWins -> "Hasil:\n${listOfPlayers[0].name} MENANG! 🎉"
+            playerTwoWins -> "Hasil:\n${listOfPlayers[1].name} MENANG! 🎊"
+            else -> "Hasil:\nSERI!!!1!1!1! 😱"
+        }
+    }
 }
