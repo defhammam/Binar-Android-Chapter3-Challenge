cd1ac47a160a4c7ca6f1fa45888c05e8a01ceca1
Hammam <hammam.mahfuzh.s@mail.ugm.ac.id>
2022-09-05 00:51:42 +0700

    String input validator created

--- src/main/kotlin/utils/IOUtils.kt
+++ src/main/kotlin/utils/IOUtils.kt
@@ -2,4 +2,11 @@ package utils
 import model.Player
 
 object IOUtils {
+    fun getHand(default: String = ""): String? {
+        val choice: String = readLine()?.lowercase() ?: default
+        return if (Player().possibleChoices.contains(choice))
+            choice
+        else
+            null
+    }
 }
